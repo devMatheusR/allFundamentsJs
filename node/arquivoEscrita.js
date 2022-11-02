7053524f570c1236e3399e08bfc172c3fc4e2d25
Matheus Ramos <devmatheusr@gmail.com>
2022-11-02 17:45:10 -0300

    backend com express

--- node/arquivoEscrita.js
+++ node/arquivoEscrita.js
@@ -6,8 +6,19 @@ const produto = {
     desconto: 0.15
 }
 
+const veiculo = {
+    nome: 'Carro',
+    preco: 50000,
+    desconto: 0
+}
+
 
 fs.writeFile(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err => {
+    console.log(err || 'Arquivo salvo!')
+})
+
+
+fs.writeFile(__dirname + '/arquivoGerado2.json', JSON.stringify(veiculo), err => {
     console.log(err || 'Arquivo salvo!')
 })
 
